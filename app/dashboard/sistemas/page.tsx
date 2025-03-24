d770f5eba4d7c64f9c7b6ad46c3df32ead6c393e
Hensley Ferrari <hensley_ferrari@hotmail.com>
2025-03-24 20:22:27 -0300

    add tabs component and integrate into sistemas page

--- app/dashboard/sistemas/page.tsx
+++ app/dashboard/sistemas/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Sistemas = () => {
   return (
-    <div className="flex flex-col w-svh">
+    <div className="flex flex-col w-svh gap-5">
       <div className="justify-between flex">
         <h1 className="font-bold text-6xl">Sistemas </h1>
       </div>
+      <Tabs defaultValue="sistemas" className="w-full">
+        <TabsList>
+          <TabsTrigger value="sistemas">Sistemas</TabsTrigger>
+          <TabsTrigger value="meuSistemas">Meus sistemas</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sistemas">Lista dos sistemas</TabsContent>
+        <TabsContent value="meuSistemas">Lista dos seus sistemas.</TabsContent>
+      </Tabs>
     </div>
   );
 };
