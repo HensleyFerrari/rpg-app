8655b581f83460e9dc7efba539da49bec073e60b
Hensley Ferrari <hensley_ferrari@hotmail.com>
2025-04-21 11:31:59 -0300

    feat: implement authentication check and redirect for AdminPage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,16 @@
-const AdminPage = () => {
-  return <div>Teste admin</div>;
-};
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+async function AdminPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  return <div>Admin Dashboard</div>;
+}
 
 export default AdminPage;
